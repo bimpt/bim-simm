01e589bc114732e9862474c58caad7416a0a2651
sarwono <scqolbu@gmail.com>
2023-03-06 17:41:48 +0700

    navbar

--- resources/views/layouts/templates/simm/navigations/navbar.blade.php
+++ resources/views/layouts/templates/simm/navigations/navbar.blade.php
@@ -1,1 +1,18 @@
-s
+<nav id="bim-navbar" class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="">
+            <img src="{{ asset('/themes/img/pengaturan-aplikasi/logo-alistiqomah.png') }}" alt="logo" class="logo-brand">
+        </a>
+        <div class="menu">
+            <a class="nav-link" href="">Profil</a>
+            <a class="nav-link" href="">Keuangan</a>
+            <a class="nav-link" href="">Kegiatan</a>
+            <a class="nav-link" href="">Layanan</a>
+            <a class="nav-link" href="">Konten & Galeri</a>
+        </div>
+
+        <div class="btn btn-navbar">
+            <a style="color: white; text-decoration: none" href="">Donasi</a>
+        </div>
+    </div>
+</nav>
--- resources/views/pages/guest/landingpage.blade.php
+++ resources/views/pages/guest/landingpage.blade.php
@@ -1,3 +1,13 @@
 <x-simm-app-layout title="Beranda">
-test
+    <!-- header -->
+    <section id="bim-header">
+        <div class="header-img">
+            <img src="/themes/img/pengaturan-aplikasi/header.png" alt="Lorem Ipsum">
+            <div class="slogan">
+              {{-- <h1>Selamat Datang di Website Masjid Al Istiqomah</h1> --}}
+              {{-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> --}}
+            </div>
+          </div>
+    </section>
 </x-simm-app-layout>
